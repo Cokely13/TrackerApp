87b050486e7540487c44cd378433504ee1854751
Ryan Cokely <ryan.cokely@gmail.com>
2023-01-19 17:37:59 -0600

    background css

--- client/App.js
+++ client/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import 'bootstrap'
 import Navbar from './components/Navbar'
 import Routes from './Routes'
+// import './index.css'
 
 
 const App = () => {
   return (
-    <div className="p-3 mb-2 bg-dark-subtle text-emphasis-dark">
+    <div className="bg-success p-2 text-dark bg-opacity-10"
+     >
       <Navbar />
       <Routes />
     </div>
